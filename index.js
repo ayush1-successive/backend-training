a8c9e4ba0d79474def3324a9811516a3dd1dcaa4
ayush1-successive <ayush.sinha@successive.tech>
2023-11-16 13:19:19 +0530

    Converted to ES-module

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const math = require("./lib/math");
-const fs = require("fs");
+import { add, sub, mult, div } from "./lib/math.js";
+import { appendFileSync } from "fs";
 
 const doMath = (num1, num2) => {
   const csvData = [
     ["Operations", "num1", "num2", "Result"],
-    ["Addition", num1, num2, math.add(num1, num2)],
-    ["Subtraction", num1, num2, math.sub(num1, num2)],
-    ["Multiplication", num1, num2, math.mult(num1, num2)],
-    ["Division", num1, num2, math.div(num1, num2)],
+    ["Addition", num1, num2, add(num1, num2)],
+    ["Subtraction", num1, num2, sub(num1, num2)],
+    ["Multiplication", num1, num2, mult(num1, num2)],
+    ["Division", num1, num2, div(num1, num2)],
   ];
 
   const csvString = csvData.map((row) => row.join(", ")).join("\n") + "\n\n";
-  fs.appendFileSync("Maths Operation Result.xlsx", csvString, "utf-8");
+  appendFileSync("Maths Operation Result.xlsx", csvString, "utf-8");
 };
 
 console.log(process.argv);
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-if (arguments.length < 2) {
+if (args.length < 2) {
   console.log("Invalid argument!");
 } else {
-  const num1 = Number(arguments[0]);
-  const num2 = Number(arguments[1]);
+  const num1 = Number(args[0]);
+  const num2 = Number(args[1]);
 
   console.log(num1, num2);
 
   if (!(num1 && num2)) {
     console.log("undefined values passed!");
-    return;
+  } else {
+    doMath(num1, num2);
   }
-
-  doMath(num1, num2);
 }
