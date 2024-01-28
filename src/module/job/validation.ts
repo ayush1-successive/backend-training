e79c9af417b218ca117065fb8110dc2da2beafce
ayush1-successive <ayush.sinha@successive.tech>
2024-01-28 16:56:34 +0530

    Minor updates

--- src/module/job/validation.ts
+++ src/module/job/validation.ts
@@ -6,20 +6,20 @@ const jobValidation: ObjectSchema<IJobListing> = joi.object({
     company: joi.string().required(),
     address: joi.object(),
     jobType: joi.string().required(),
-    industry: joi.string(),
+    industry: joi.string().required(),
     description: joi.string(),
     requirements: joi.array(),
     responsibilities: joi.array(),
     qualifications: joi.object(),
     salary: joi.number().integer(),
-    applicationDeadline: joi.date(),
-    isRemote: joi.boolean(),
+    applicationDeadline: joi.date().required(),
+    isRemote: joi.boolean().required(),
     contactEmail: joi
         .string()
         .email({ tlds: { allow: false } })
         .trim()
         .required(),
-    applicationLink: joi.string(),
+    applicationLink: joi.string().required(),
 });
 
 export default jobValidation;
